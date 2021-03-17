473922c066f8b798557d9ccf29079896ccacaac5
chemoontheshy <xzfandzgx@gmail.com>
2021-03-17 17:13:55 +0800

    test

--- testDemo1/app/src/main/java/com/qchemmo/testdemo1/MainActivity.kt
+++ testDemo1/app/src/main/java/com/qchemmo/testdemo1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.qchemmo.testdemo1
 
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.net.Uri.parse
 import androidx.appcompat.app.AppCompatActivity
@@ -7,10 +8,12 @@ import android.os.Bundle
 import android.os.Environment
 import android.util.Log
 import android.widget.MediaController
+import android.widget.Toast
 import android.widget.VideoView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 
+const val REQUEST_WRITE_EXTERNAL_STORAGE = 1
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,14 +26,39 @@ class MainActivity : AppCompatActivity() {
         val mediaController = MediaController(this)
         mediaController.setAnchorView(videoView)
         //specify the location of media file
-
-        Log.e("te",Environment.getExternalStorageState().toString())
-        val uri:Uri = parse(getExternalFilesDir(null)?.path+"/test.mp4")
+        handlePermissions()
+        //val uri:Uri = parse(getExternalFilesDir("test.mp4")?.path)
+        val uri:Uri = parse(Environment.getExternalStoragePublicDirectory("/kotlin/test.mp4").path)
         Log.e("uri",uri.toString())
         videoView.setMediaController(mediaController)
         videoView.setVideoURI(uri)
         videoView.requestFocus()
         videoView.start()
+    }
+
+    private fun handlePermissions(){
+        val permission = android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        val checkSelfPermission = this.checkSelfPermission(permission)
+        if (checkSelfPermission==PackageManager.PERMISSION_GRANTED){
+            Log.e("permission","have permission")
+        }
+        else{
+            requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                REQUEST_WRITE_EXTERNAL_STORAGE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            REQUEST_WRITE_EXTERNAL_STORAGE->{
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this,"已申请权限",Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this,"申请权限失败",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
     /**
---
+++ testDemo1/app/src/main/java/com/qchemmo/testdemo1/utils/PermissionsUtils.kt
@@ -0,0 +1,20 @@
+package com.qchemmo.testdemo1.utils
+
+import android.content.pm.PackageManager
+import android.widget.Toast
+import java.security.AccessControlContext
+import java.util.jar.Manifest
+
+/**
+ * @ClassName: Permissions
+ * @Description:
+ * @Author: chemoontheshy
+ * @Date: 2021/3/17-15:18
+ */
+
+const val REQUEST_WRITE_EXTERNAL_STORAGE = 1
+
+abstract class PermissionsUtils() {
+
+
+}
